209a7c11c13770c8af819e693b40071ae937f06f
Abhishek Patel <64314222+imdarkcoder@users.noreply.github.com>
2020-11-05 16:21:40 +0530

    Source Code Uploaded

---
+++ OS-info.py
@@ -0,0 +1,16 @@
+import platform 
+sys = platform.uname()
+"Abhishek Patel @imdarkcoder"
+
+print(f"System: {sys.system}")
+
+print(f"Node Name: {sys.node}")
+
+print(f"Release: {sys.release}")
+
+print(f"Version: {sys.version}")
+
+print(f"Machine: {sys.machine}")
+
+print(f"Processor: {sys.processor}")
+
